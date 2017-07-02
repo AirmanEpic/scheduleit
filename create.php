6b485f522d03471750caeab5fb6cd5a63ce99d95
Stefan Blandin <stefanblandin@yahoo.com>
2017-07-02 15:08:08 -0700

    Basic CSS for Create.php

--- create.php
+++ create.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title></title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:700i|Roboto:300" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="main.css">
 	<script src="jquery-2.1.4.min.js"></script>
 	<script src="jquery.color-2.1.2.min.js"></script>
@@ -9,7 +10,8 @@
 <body>
 <section class="center">
 	<h1> Create new operation </h1>
-	<h2> Name: </h2>
+	<div class="horbox" style="margin:16px"><h2 style="margin-right:16px"> Name: </h2> <input class="namein" placeholder="Cool New thing"></div>
+	<br>
 	<div class="button createbutton"><p>Create</p></div>
 	<p class="accesshere">Access URL: please wait</p>
 	<p class="deletehere">Delete URL: please wait</p>
