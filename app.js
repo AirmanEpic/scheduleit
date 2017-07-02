88cc448994a7722ac0135199f1511e484ef3297e
Stefan Blandin <stefanblandin@yahoo.com>
2017-07-02 13:10:31 -0700

    Initial Commit
    Creating base files

---
+++ app.js
@@ -0,0 +1,35 @@
+var main=function(){
+
+}
+
+function detectmob() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
+window.onresize = function(event) {
+resizeDiv();
+}
+
+
+function resizeDiv() {
+	vpw = $(window).width();
+	vph = $(window).height();
+
+	m=detectmob()
+}
+
+
+$(document).ready(main)
+$(document).ready(resizeDiv)
